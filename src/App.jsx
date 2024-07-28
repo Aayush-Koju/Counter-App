2f1b1bc6f4bc4d674b3b85860d4b12b5ac1b80cb
Aayush-Koju <iush.cozu12@gmail.com>
2024-07-28 13:31:54 +0545

    implemented feature for custom increment and decrement

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,30 @@ import {useState} from "react";
 function App() {
   
   const [count, setCounter] = useState(0);
+  const [num, setNum] = useState(1);
 
   const increase = () => {
-    setCounter(count + 1);
+    setCounter(count + num);
   }
 
   const decrease = () => {
-    setCounter(count - 1);
+    setCounter(count - num);
   }
 
   const clear = () => {
     setCounter(0);
   }
 
+  const changeNum = (e) => {
+    setNum(e.target.value);
+  }
+
   return (
     <>
-      <div>
-        counter: {count}
-      </div> 
+      counter: {count}
+
       <button onClick={increase}>Increment(+)</button>
+      <input type="number" value={num} onChange={changeNum}/>
       <button onClick={decrease}>Decrement(-)</button>
       <button onClick={clear}>Reset</button>
     </>
